970b640bf1670721742c0dd66d39c38eb0c62af5
Vrinda Aggarwal <96989608+aggarwalvrinda@users.noreply.github.com>
2023-08-23 08:01:13 +0530

    Create bipartite_graph.java

---
+++ bipartite_graph.java
@@ -0,0 +1,44 @@
+/* The graph is said to be bipartite if all the nodes can be divided into 2 mutually exclusive and exhaustive sets such 
+that all edges are across sets there are a few ways to figure out if a graph is bipartite or not 
+eg
+
+0 ----- 1
+|       |
+|       |
+|       |
+3-------2   this graph is bipartite if the graph is acyclic and if cyclic have even edges then it is 
+*/
+
+// method 1: BFS
+/*in this methodwe will color th first node with blue and the adject one with red if the 2 adjecent nodes are of 
+same color then they are not bipartite 
+*/
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int len = graph.length;
+        int color[] = new int[len];
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=0;i<len;i++){
+             if (color[i] != 0) { // going to everynode as the graph can be a cyclic
+                continue; // Node is already colored
+            }
+            q.add(i);
+            color[i]=1;
+            while(!q.isEmpty()){
+                int curr = q.poll();
+                for(int next : graph[curr]){
+                     if(color[next]==0){
+                    color[next]= -color[curr];
+                    q.add(next);
+                    }
+                    if(color[next] == color[curr]) return false;
+                }
+               
+                
+            }
+        }
+        return true;
+
+    }
+}
