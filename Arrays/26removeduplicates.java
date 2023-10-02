9f6c261774be44b327038da9173bfb3bb88e0e4e
Vrinda Aggarwal <96989608+aggarwalvrinda@users.noreply.github.com>
2023-10-03 01:43:11 +0530

    Create 26removeduplicates.java

---
+++ Arrays/26removeduplicates.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1; // this is the length which will be i+1 bay i =0,1,2 total length is 3
+    }
+}
