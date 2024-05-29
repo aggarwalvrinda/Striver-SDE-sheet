ae45de6e8e7949ef35829411c556fd341aa0751f
Vrinda Aggarwal <96989608+aggarwalvrinda@users.noreply.github.com>
2024-05-29 13:00:44 +0530

    Update topologicalSort.java

--- graphs/topologicalSort.java
+++ graphs/topologicalSort.java
@@ -37,3 +37,51 @@ class Solution
         return result;
     }
 }
+
+//BFS 
+//Kahns algorithm it depends on the indegree maintain the indegree
+
+class Solution
+{
+    //Function to return list containing vertices in Topological order.
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        int N = V;
+        int[] indegree = new int[V];
+        Queue <Integer> q = new LinkedList<>();
+        
+        // fill indegree with values
+        for(int u=0;u<N;u++){
+            for(int v : adj.get(u)){
+                indegree[v]++;
+            }
+        }
+        
+        //push in queue with indegree =0
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)
+            q.add(i);
+        }
+        // bfs
+        
+        int[] res = new int[V];
+        int index = 0;
+        while(!q.isEmpty()){
+             int u = q.poll();  // Use poll to get and remove the front element
+            
+            res[index++] = u;
+            for(int v: adj.get(u)){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.add(v);
+                }
+                
+            }
+        }
+        return res;
+        
+        
+        
+    }
+}
