6f98be861d533ce37d393f050b6b7e88b1a17269
Vrinda Aggarwal <96989608+aggarwalvrinda@users.noreply.github.com>
2024-06-25 16:16:03 +0530

    Update rightView.java

--- Trees/rightView.java
+++ Trees/rightView.java
@@ -27,3 +27,39 @@ class Solution {
         return result ;
     }
 }
+
+// Dfs 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ // using dfs / preorder
+class Solution {
+    public void preOrder(TreeNode root, int n , ArrayList<Integer> result){
+        if(root==null){
+            return ;
+        }
+        if(result.size()<n){
+            result.add(root.val);
+        }
+        preOrder(root.right, n+1, result);
+        preOrder(root.left, n+1, result);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        preOrder(root, 1, result);
+        return result;
+    }
+}
